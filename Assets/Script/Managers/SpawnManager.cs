20032be396f38b8f6a220b7f16f80421a081344f
Anthony <saterny@gmail.com>
2018-03-30 14:19:39 -0700

    Spawn Manager added
    Added a manager to control the spawning of Enemy prefabs every X seconds.

---
+++ Assets/Script/Managers/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+    public GameObject enemyPrefab;          // prefab to spawn
+	public float spawnTime = 6f;            // how long between each spawn
+    private Vector3 spawnPosition;
+
+    // Use this for initialization
+    void Start() {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn() {
+        spawnPosition.x = Random.Range(-17, 17);
+        spawnPosition.y = 0.5f;
+        spawnPosition.z = Random.Range(-17, 17);
+
+        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/Script/Managers/StatisticsManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatisticsManager : MonoBehaviour {
+
+    public int maxHealth;
+    public int currentHealth;
+
+    public float moveSpeed = 10f;
+
+    public GameObject bulletGO;
+
+    public int CurrentHealth {
+        get {
+            return currentHealth;
+        }
+
+        set {
+            currentHealth = value;
+        }
+    }
+
+    public int MaxHealth {
+        get {
+            return maxHealth;
+        }
+
+        set {
+            maxHealth = value;
+        }
+    }
+
+    public float MoveSpeed {
+        get {
+            return moveSpeed;
+        }
+
+        set {
+            moveSpeed = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start() {
+        CurrentHealth = MaxHealth;
+    }
+
+    private void FixedUpdate() {
+        if (currentHealth <= 0) {
+            Debug.Log("Destroying object " + gameObject.name);
+            Destroy(this.gameObject);
+        }
+    }
+
+    //Call this to deal damage to the object this is attached to.
+    public void DealDamageToObject(int damage) {
+        Debug.Log("Dealing " + damage.ToString() + " to HP pool: " + currentHealth);
+        this.currentHealth -= damage;
+        Debug.Log("Health is now" + currentHealth);
+    }
+
+}
