3dc97814c28e5267465ebc5afbb4349023f5781b
Anthony <saterny@gmail.com>
2018-03-30 17:22:15 -0700

    Added Enemy Line of Sight and Aggression mechanics
    Enemies now check for a player entering their line of sight, and will start to chase upon seeing a player.
    
    Additionally, enemies will lose line of sight and go passive if line of sight is blocked by a wall.

--- Assets/Script/Enemies/NonPlayerController.cs
+++ Assets/Script/Enemies/NonPlayerController.cs
@@ -8,7 +8,7 @@ public class NonPlayerController : MonoBehaviour {
     public Transform goal;
     NavMeshAgent agent;
 
-    public bool isPassive = false;
+    public bool isPassive = true;
 
     // Use this for initialization
     void Start () {
@@ -18,8 +18,30 @@ public class NonPlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        //if we have line of sight and aggression towards player
         if (!isPassive) {
+            //enable the chasing behavior
+            agent.isStopped = false;
             agent.destination = goal.position;
+
+            //check if we lost line of sight and passify agent if so
+            if (Physics.Linecast(transform.position, goal.position)) {
+                Debug.Log("Line of sight lost");
+                isPassive = true;
+                agent.isStopped = true;
+            }
+        }
+        else {
+        }
+    }
+
+    public void CheckVisibility(Collider target) {
+        //check if we have line of sight to target
+        if (Physics.Linecast(transform.position, target.transform.position)) {
+        }
+        else {
+            Debug.Log("Activating aggression");
+            isPassive = false;
         }
     }
 }
---
+++ Assets/Script/LineOfSightScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSightScript : MonoBehaviour {
+
+    NonPlayerController parentController;
+
+    void Start() {
+        parentController = GetComponentInParent<NonPlayerController>();
+    }
+
+    //pass collision trigger on to parent
+    private void OnTriggerStay(Collider other) {
+        if (parentController.isPassive) {
+            parentController.CheckVisibility(other);
+        }
+
+    }
+}
