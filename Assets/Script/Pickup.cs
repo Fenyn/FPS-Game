b0d112a50a4e33ed1e74c46fe0da946cb1d7116f
Anthony <saterny@gmail.com>
2018-04-01 14:45:47 -0700

    Importing many, many assets
    Building prefab bundle from Unity store is now included. Starting construction of starting town

--- Assets/Script/Pickup.cs
+++ Assets/Script/Pickup.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour {
 
-    CollectableManager collectableManager;
     string[] possiblePickupTypes = new string[] { "Apple", "Bread", "Meat", "Wood"};
-
     public string currentItemType;
-    public float rotateSpeed = 50f;
+    CollectableManager collectableManager;
 
+    //adjust this to change how fast it rotates
+    public float rotateSpeed = 50f;
     //adjust this to change speed
     public float speed = 1f;
     //adjust this to change how high it goes
     public float height = 0.5f;
+    //adjust this to change the minimum float height
+    public float minFloatHeight = 1.5f;
 
-    // Use this for initialization
     void Start () {
         collectableManager = GameObject.Find("CollectableManager").GetComponent<CollectableManager>();
 	}
@@ -26,21 +27,22 @@ public class Pickup : MonoBehaviour {
         //calculate what the new Y position will be
         float newY = Mathf.Sin(Time.time * speed);
         //set the object's Y to the new calculated Y
-        transform.position = new Vector3(pos.x, 1.5f + newY * height, pos.z) ;
+        transform.position = new Vector3(pos.x, minFloatHeight + newY * height, pos.z) ;
         transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed, Space.World);
     }
 
     private void OnTriggerEnter(Collider other) {
-        Debug.Log("Trigger entered. Other object: " + other.gameObject);
         collectableManager.PickUpItem(currentItemType);
-        //Destroy(this.gameObject);
+        Destroy(this.gameObject);
     }
 
     void OnMouseEnter() {
+        //Set object to a layer where it will have an outline
         gameObject.layer = 8;
     }
 
     void OnMouseExit() {
+        //Set object to a layer where it will no longer have an outline
         gameObject.layer = 0;
     }
 }
