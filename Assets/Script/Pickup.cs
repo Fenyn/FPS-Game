79af7585ccc453d8efe2cea91d62e095d1b2d4c8
Anthony <saterny@gmail.com>
2018-03-31 17:40:41 -0700

    Expanding Pickups
    Pickups now rotate and bob up and down gently. Additionally, pickups now have an outline when hovered over.

--- Assets/Script/Pickup.cs
+++ Assets/Script/Pickup.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 public class Pickup : MonoBehaviour {
 
     CollectableManager collectableManager;
-    public string[] possiblePickupTypes = new string[] { "Apple", "Bread", "Meat", "Wood"};
-    public string currentItemType;
+    string[] possiblePickupTypes = new string[] { "Apple", "Bread", "Meat", "Wood"};
 
-	// Use this for initialization
-	void Start () {
+    public string currentItemType;
+    public float rotateSpeed = 50f;
+
+    //adjust this to change speed
+    public float speed = 1f;
+    //adjust this to change how high it goes
+    public float height = 0.5f;
+
+    // Use this for initialization
+    void Start () {
         collectableManager = GameObject.Find("CollectableManager").GetComponent<CollectableManager>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void Update() {
+        //get the objects current position and put it in a variable so we can access it later with less code
+        Vector3 pos = transform.position;
+        //calculate what the new Y position will be
+        float newY = Mathf.Sin(Time.time * speed);
+        //set the object's Y to the new calculated Y
+        transform.position = new Vector3(pos.x, 1.5f + newY * height, pos.z) ;
+        transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed, Space.World);
+    }
 
     private void OnTriggerEnter(Collider other) {
         Debug.Log("Trigger entered. Other object: " + other.gameObject);
         collectableManager.PickUpItem(currentItemType);
-        Destroy(this.gameObject);
+        //Destroy(this.gameObject);
+    }
+
+    void OnMouseEnter() {
+        gameObject.layer = 8;
+    }
+
+    void OnMouseExit() {
+        gameObject.layer = 0;
     }
 }
